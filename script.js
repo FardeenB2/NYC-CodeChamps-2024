a57e52024c9d47f3151ed31a988fc65d9c12f74c
Fardeen Bhuiyan <fardeenbhuiyan@Fardeens-Air.lan>
2024-07-18 16:52:39 -0400

    added coloring font

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//This is for the toggle nav menu
 const menu = document.querySelector('#mobile-menu');        //calls id mobile-menu from home.html
-const menuLinks = document.querySelector('.navbar_menu');      //assigns this menuLinks variable to be the container menu of navigation bar.
+const menuLinks = document.querySelector('.navbar_menu');      
 
 menu.addEventListener('click', function() {     //allows button action of the toggle menu button when it percieves a 'click'
     menu.classList.toggle('is-active');         //if toggle is clicked, is-active activates and the hamburger icon turns into an X.
-    menuLinks.classList.toggle('active');       //if toggle is clicked, this enables the showing or hiding of the menu.
+    menuLinks.classList.toggle('active');       //if toggle is clicked, this enables the revealing or concealing of the menu.
 });

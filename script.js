bfc671071e091ad61f22c753f709a02f24d58db6
Avisek Pandit <simonp4466@gmail.com>
2024-07-24 14:52:30 -0400

    added toggle faq questions

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-//This is for the toggle nav menu
-const menu = document.querySelector('#mobile-menu');        //calls id mobile-menu from home.html
-const menuLinks = document.querySelector('.navbar_menu');      
+document.addEventListener("DOMContentLoaded", function() { 
+  const menu = document.querySelector('#mobile-menu');        //calls id mobile-menu from home.html
+  const menuLinks = document.querySelector('.navbar_menu');      
 
-menu.addEventListener('click', function() {     //allows button action of the toggle menu button when it percieves a 'click'
-    menu.classList.toggle('is-active');         //if toggle is clicked, is-active activates and the hamburger icon turns into an X.
-    menuLinks.classList.toggle('active');       //if toggle is clicked, this enables the revealing or concealing of the menu.
-});
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('is-visible');
-      } else {
-        entry.target.classList.remove('is-visible');
-      }
-    });
+  menu.addEventListener('click', function() {     //allows button action of the toggle menu button when it percieves a 'click'
+      menu.classList.toggle('is-active');         //if toggle is clicked, is-active activates and the hamburger icon turns into an X.
+      menuLinks.classList.toggle('active');       //if toggle is clicked, this enables the revealing or concealing of the menu.
   });
-  
-  const h3ToFade = document.querySelector('.fade-on-scroll');
-  observer.observe(h3ToFade);
+
+  const faqQuestions = document.getElementsByClassName('faq-text-container')
+  Array.from(faqQuestions).forEach(function(qst) {
+    qst.addEventListener('click', function(e) {
+      if (e.target.className == 'faq-question') {
+        Array.from(faqQuestions).forEach(function(ele) {
+          ele.id = "faq-close"
+          ele.children[0].children[0].setAttribute("id", "faq-closed-text")
+        })
+      }
+      e.target.parentNode.id = "faq-open"
+      e.target.children[0].setAttribute("id", "faq-open-text")
+    })
+  })
+
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+        } else {
+          entry.target.classList.remove('is-visible');
+        }
+      });
+    });
+    
+    const h3ToFade = document.querySelector('.fade-on-scroll');
+    observer.observe(h3ToFade);
+
+});

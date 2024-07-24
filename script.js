efad79933b92975bd4b744a8a47f071bc4216736
deadjar <jeffsarrax@gmail.com>
2024-07-24 11:47:28 -0400

    Added About details

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ menu.addEventListener('click', function() {     //allows button action of the to
     menu.classList.toggle('is-active');         //if toggle is clicked, is-active activates and the hamburger icon turns into an X.
     menuLinks.classList.toggle('active');       //if toggle is clicked, this enables the revealing or concealing of the menu.
 });
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+      } else {
+        entry.target.classList.remove('is-visible');
+      }
+    });
+  });
+  
+  const h3ToFade = document.querySelector('.fade-on-scroll');
+  observer.observe(h3ToFade);
